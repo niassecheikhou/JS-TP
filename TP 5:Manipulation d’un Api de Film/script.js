ab48ee6a0435b1222ba332cbaf314e65e2531311
Niasse cheikhou <niassecheikhou@gmail.com>
2022-03-24 09:54:00 +0100

    seconde commit

--- TP 5:Manipulation d’un Api de Film/script.js
+++ TP 5:Manipulation d’un Api de Film/script.js
@@ -1,14 +1,16 @@
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 fetch(APIURL).then(response => {
+   
     response.json().then(data=>{
         
         data.results.forEach(elements=>{
-    
+            console.log(data.results);
             var titlefilm=elements['original_title']
             var photofilm=elements['poster_path']
             var overviewfilm=elements['overview']
             var votefilm=elements['vote_average']
+        
             
             console.log(photofilm);
 
